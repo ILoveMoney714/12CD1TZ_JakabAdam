8ae19ae737cf3f249932912d06c3fef7389b005d
ILoveMoney714 <jakada565@hengersor.hu>
2025-02-27 09:26:36 +0100

    db kapcsolat létrehozása

--- teliolimpia.js
+++ teliolimpia.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require("cors");
+const mysql = require("mysql");
+app.use(bodyParser.json());
 
 
-
+app.use(cors())
 app.use(bodyParser.json())
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3307,
+    password: "",
+    database: "teliolimpia",
+}); 
 
 app.get("/", (req, res) => {
     res.send("Működik a szerver.");
