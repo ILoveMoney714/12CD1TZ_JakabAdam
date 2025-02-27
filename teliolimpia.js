72aec37f27d04adabf2366fd00ff0d6414fa40aa
ILoveMoney714 <jakada565@hengersor.hu>
2025-02-27 09:39:47 +0100

    vége

--- teliolimpia.js
+++ teliolimpia.js
@@ -17,10 +17,19 @@ const db = mysql.createConnection({
     database: "teliolimpia",
 }); 
 
+
+
 app.get("/", (req, res) => {
     res.send("Működik a szerver.");
 })
 
+app.get("/v", (req, res) => {
+    const sql = "SELECT * FROM `versenyzok`";
+    db.query(sql, (err, result) => {
+        if (err) return res.status(500).json({error: err.message});
+        return res.json(results)
+    })
+})  
 
 app.listen(3000, () => {
     console.log("A téliolimpia szervere a 3000-es porton fut");
